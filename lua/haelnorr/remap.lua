aa8ed34862b63cb295b69a8b3657e6fd701aca96
Locke D <lockedonohoe@gmail.com>
2024-06-16 11:45:29 +1000

    Added Suda, nvim-surround, colorcolumn toggle

--- lua/haelnorr/remap.lua
+++ lua/haelnorr/remap.lua
@@ -1,6 +1,8 @@
 vim.g.mapleader = " "
+-- directory viewer
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- move highlighted lines up and down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
@@ -10,19 +12,17 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- greatest remap ever
+-- pastes without replacing buffer
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
--- next greatest remap ever : asbjornHaland
+-- yanks to system clipboard
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
+-- pretty sure this deletes without replacing buffer but idk
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
--- This is going to get me cancelled
-vim.keymap.set("i", "<C-c>", "<Esc>")
-
-vim.keymap.set("n", "Q", "<nop>")
+-- idk something with tmux, ill come back to this
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
@@ -37,3 +37,14 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
+
+vim.keymap.set("n", "oo", "o<Esc>k")
+
+vim.keymap.set("n", "<C-Q>", function()
+    if vim.inspect(vim.opt.colorcolumn:get()) == "{}" then
+        vim.opt.colorcolumn = "80"
+    else
+        vim.opt.colorcolumn = ""
+    end
+end)
+
