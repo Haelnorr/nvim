f0aae9460b49d14d9586c8a4ea02bfbaddafb693
Locke D <lockedonohoe@gmail.com>
2024-06-17 22:13:55 +1000

    Added feline, customised plugins to use catppuccin

--- lua/haelnorr/lazy/colors.lua
+++ lua/haelnorr/lazy/colors.lua
@@ -1,6 +1,40 @@
 return {
     "catppuccin/nvim",
     config = function()
+        require("catppuccin").setup({
+            integrations = {
+                gitsigns = true,
+                treesitter = true,
+                harpoon = true,
+                mason = true,
+                cmp = true,
+                native_lsp = {
+                    enabled = true,
+                    virtual_text = {
+                        errors = { "italic" },
+                        hints = { "italic" },
+                        warnings = { "italic" },
+                        information = { "italic" },
+                        ok = { "italic" },
+                    },
+                    underlines = {
+                        errors = { "underline" },
+                        hints = { "underline" },
+                        warnings = { "underline" },
+                        information = { "underline" },
+                        ok = { "underline" },
+                    },
+                    inlay_hints = {
+                        background = true,
+                    },
+                },
+                fidget = true,
+                telescope = {
+                    enabled = true,
+                },
+                lsp_trouble = true,
+            },
+        })
         vim.cmd('colorscheme catppuccin-mocha')
     end,
 }
