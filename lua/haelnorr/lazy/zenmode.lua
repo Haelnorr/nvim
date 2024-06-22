d4573f2a0495b48d4c266f2a26b5b28a0b55e90e
Locke D <lockedonohoe@gmail.com>
2024-06-22 14:29:12 +1000

    Removed superzenmode toggle

--- lua/haelnorr/lazy/zenmode.lua
+++ lua/haelnorr/lazy/zenmode.lua
@@ -14,21 +14,6 @@ return {
             vim.wo.number = true
             vim.wo.rnu = true
         end)
-
-
-        vim.keymap.set("n", "<leader>zZ", function()
-            require("zen-mode").setup {
-                window = {
-                    width = 80,
-                    options = { }
-                },
-            }
-            require("zen-mode").toggle()
-            vim.wo.wrap = false
-            vim.wo.number = false
-            vim.wo.rnu = false
-            vim.opt.colorcolumn = "0"
-        end)
     end
 }
 
