4bae9ab7a3626efefbda72ea73d311c61ea7de00
Haelnorr <lockedonohoe@gmail.com>
2024-06-23 09:39:34 +1000

    Made bg transparent

--- lua/haelnorr/lazy/colors.lua
+++ lua/haelnorr/lazy/colors.lua
@@ -36,5 +36,7 @@ return {
             },
         })
         vim.cmd('colorscheme catppuccin-mocha')
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     end,
 }
