a15ac241ef2f3d95375155d83b8fe5833acd560a
Haelnorr <lockedonohoe@gmail.com>
2025-01-23 01:16:45 +1100

    Updated aerial config

--- lua/haelnorr/lazy/aerial.lua
+++ lua/haelnorr/lazy/aerial.lua
@@ -1,21 +1,33 @@
 return {
-    "stevearc/aerial.nvim",
-    opts = {},
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter",
-        "nvim-tree/nvim-web-devicons"
-    },
-    config = function()
-        require("aerial").setup({
-            on_attach = function(bufnr)
-                vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
-                vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
-            end,
-            layout = {
-                default_direction = "prefer_left",
-                min_width = 10
-            }
-        })
-        vim.keymap.set("n", "<leader>pa", "<cmd>AerialToggle!<CR>")
-    end
+	"stevearc/aerial.nvim",
+	opts = {},
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		require("aerial").setup({
+			on_attach = function(bufnr)
+				vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+				vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+			end,
+			layout = {
+				default_direction = "prefer_left",
+				min_width = 30,
+				width = nil,
+				max_width = { 40, 0.2 },
+				preserve_equality = true,
+			},
+			filter_kind = {
+				"Class",
+				"Constructor",
+				"Enum",
+				"Function",
+				"Interface",
+				"Module",
+				"Method",
+			},
+		})
+		vim.keymap.set("n", "<leader>pa", "<cmd>AerialToggle!<CR>")
+	end,
 }
