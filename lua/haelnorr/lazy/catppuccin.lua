31c306248e9fff6959c7fd6af3ee26ceff937503
Haelnorr <lockedonohoe@gmail.com>
2025-02-15 09:17:19 +1100

    Replaced feline with lualine. o7 feline, you will be missed

---
+++ lua/haelnorr/lazy/catppuccin.lua
@@ -0,0 +1,43 @@
+return {
+	"catppuccin/nvim",
+	config = function()
+		require("catppuccin").setup({
+			integrations = {
+				aerial = true,
+				cmp = true,
+				fidget = true,
+				gitsigns = true,
+				harpoon = true,
+				mason = true,
+				lsp_trouble = true,
+				native_lsp = {
+					enabled = true,
+					virtual_text = {
+						errors = { "italic" },
+						hints = { "italic" },
+						warnings = { "italic" },
+						information = { "italic" },
+						ok = { "italic" },
+					},
+					underlines = {
+						errors = { "underline" },
+						hints = { "underline" },
+						warnings = { "underline" },
+						information = { "underline" },
+						ok = { "underline" },
+					},
+					inlay_hints = {
+						background = true,
+					},
+				},
+				telescope = {
+					enabled = true,
+				},
+				treesitter = true,
+			},
+		})
+		vim.cmd("colorscheme catppuccin-mocha")
+		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	end,
+}
